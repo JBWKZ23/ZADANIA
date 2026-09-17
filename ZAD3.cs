5e611b0052abdc90c9f36034ec6f71605992e85e
SkibidiSigma67 <nbubiak_231p2@e.zseeim.edu.pl>
2026-09-17 11:40:51 +0200

    Add files via upload

---
+++ ZAD3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static int ModPow(int a, int b)
+    {
+        int wynik = 1;
+        a = a % 10;
+
+        while (b > 0)
+        {
+            if (b % 2 == 1)
+            {
+                wynik = (wynik * a) % 10;
+            }
+
+            a = (a * a) % 10;
+            b = b / 2;
+        }
+
+        return wynik;
+    }
+
+    static void Main()
+    {
+        int D = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < D; i++)
+        {
+            string[] dane = Console.ReadLine().Split();
+
+            int a = int.Parse(dane[0]);
+            int b = int.Parse(dane[1]);
+
+            Console.WriteLine(ModPow(a, b));
+        }
+    }
+}
