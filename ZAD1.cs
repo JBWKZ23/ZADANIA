5894a7e393089f15bdfae7cb790e9829f0980385
SkibidiSigma67 <nbubiak_231p2@e.zseeim.edu.pl>
2026-09-17 09:22:06 +0200

    Add files via upload

---
+++ ZAD1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace cw1;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            int liczba = int.Parse(Console.ReadLine());
+            bool liczbaP = true;
+            if (liczba == 0 || liczba == 1) liczbaP = false;
+            for (int i = 2; i < liczba; i++)
+            {
+                if (liczba % i == 0)
+                {
+                    liczbaP = false;
+                    break;
+                }
+            }
+            if (liczbaP == true) Console.WriteLine("TAK");
+            else Console.WriteLine("NIE");
+        }
+    }
+}
