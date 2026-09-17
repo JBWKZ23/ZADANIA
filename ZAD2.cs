07a33f5cb2c033374a5c5df370580f324b15698a
SkibidiSigma67 <nbubiak_231p2@e.zseeim.edu.pl>
2026-09-17 10:52:27 +0200

    Add files via upload

---
+++ ZAD2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int D = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < D; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 10)
+            {
+                Console.WriteLine("0 0");
+            }
+            else
+            {
+
+                int wynik = 1;
+
+
+                for (int j = 2; j <= n; j++)
+                {
+                    wynik = (wynik * j) % 100;
+                }
+
+                Console.WriteLine($"{wynik / 10} {wynik % 10}");
+            }
+        }
+    }
+}
