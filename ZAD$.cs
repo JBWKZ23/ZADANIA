0d9f1de7d8cbae468aab8254d595ac35cfbae2e1
SkibidiSigma67 <nbubiak_231p2@e.zseeim.edu.pl>
2026-09-18 09:31:44 +0200

    Add files via upload

---
+++ ZAD$.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        while (T-- > 0)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] measurements = new int[N];
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                measurements[i] = int.Parse(Console.ReadLine());
+                sum += measurements[i];
+            }
+
+            bool[] dp = new bool[sum + 1];
+            dp[0] = true;
+
+            int currentSum = 0;
+
+            foreach (int x in measurements)
+            {
+                for (int s = currentSum; s >= 0; s--)
+                {
+                    if (dp[s])
+                    {
+                        dp[s + x] = true;
+                    }
+                }
+
+                currentSum += x;
+            }
+
+            int best = 0;
+
+            for (int s = 0; s <= sum / 2; s++)
+            {
+                if (dp[s])
+                {
+                    best = s;
+                }
+            }
+            int minSpeed = sum - 2 * best;
+            int maxSpeed = sum;
+
+            Console.WriteLine($"{minSpeed} {maxSpeed}");
+        }
+    }
+}
